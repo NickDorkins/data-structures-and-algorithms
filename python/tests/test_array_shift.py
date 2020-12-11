617d921a240864228bcfe567dcbe6414f10a061e
NickDorkins <nick.dorkins@gmail.com>
2020-12-10 23:02:28 -0800

    array-shift

--- python/tests/test_array_shift.py
+++ python/tests/test_array_shift.py
@@ -1,1 +1,16 @@
-from code_challenges.array_shift.array_shift import acme
+from challenges.array_shift.array_shift import *
+
+
+def test_array_shift_one():
+    actual = insertShiftArray([2,4,6,8], 5)
+    expected = [2,4,5,6,8]
+    assert actual == expected
+
+def test_array_shift_two():
+    actual = insertShiftArray([4,8,15,23,42], 16)
+    expected = [4,8,15,16,23,42]
+    assert actual == expected
+
+
+
+
