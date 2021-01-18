3a23ed5124d73c91ab47a49f1589a976accb45cf
NickDorkins <nick.dorkins@gmail.com>
2021-01-18 00:01:48 -0800

    dequeue

--- python/challenges/fifo_animal_shelter/fifo_animal_shelter/fifo_animal_shelter.py
+++ python/challenges/fifo_animal_shelter/fifo_animal_shelter/fifo_animal_shelter.py
@@ -15,9 +15,9 @@ class AnimalShelter():
     def enqueue(self, animal):
         new_node = Node(animal)
         if animal == "":
-            raise InvalidOperationError("Please input a valid animal.")
+            raise InvalidOperationError("User input cannot be empty.")
         elif animal != "cat" and animal != "dog":
-            raise InvalidOperationError("Please input a valid animal.")
+            raise InvalidOperationError("Valid input is either 'cat' or 'dog'.")
 
         if self.is_empty() is True:
             self.rear = new_node
@@ -27,6 +27,31 @@ class AnimalShelter():
             self.rear.next = new_node
             self.rear = new_node
             return new_node
+
+    def dequeue(self, pref):
+        current = self.front
+        if pref == "":
+            raise InvalidOperationError("Please enter your prefernce of 'cat' or 'dog'.")
+        elif self.is_empty() is True:
+            raise InvalidOperationError("All of our furry friends have found homes, please try agin later.")
+        elif pref != "cat" and pref != "dog":
+            raise InvalidOperationError("We only have 'cats' and 'dogs' at our shelter, please choose one.")
+        else:
+            while current:
+                if current == pref:
+                    return pref
+                elif current != pref:
+                    current = current.next
+                else:
+                    raise InvalidOperationError(f"We do not currently have any {pref}'s available for adoption today.")
+
+
+
+            # Loop through queue until preference value is found
+            # while pref == "cat":
+            #     if current == "cat":
+            #         current.next = self.front
+            #         return current
 
 
 
--- python/challenges/fifo_animal_shelter/tests/test_fifo_animal_shelter.py
+++ python/challenges/fifo_animal_shelter/tests/test_fifo_animal_shelter.py
@@ -13,3 +13,26 @@ def test_enqueue_values():
     actual = q.front.value
     expected = "cat"
     assert actual == expected
+
+
+def test_dequeue_values():
+    q = AnimalShelter()
+    q.enqueue("cat")
+    q.enqueue("dog")
+    q.enqueue("cat")
+    q.enqueue("dog")
+    q.dequeue("cat")
+    actual = q.front.value
+    expected = "cat"
+    assert actual == expected
+
+def test_dequeue_exception1():
+    q = AnimalShelter()
+    q.enqueue("dog")
+    q.enqueue("dog")
+    q.enqueue("dog")
+    q.enqueue("dog")
+    q.dequeue("dog")
+    actual = "We do not currently have any cat's available for adoption today."
+    expected = "cat"
+    assert actual == expected
