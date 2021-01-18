ed568c6b34a6071945b6a9c55dc369f22eeedb1e
NickDorkins <nick.dorkins@gmail.com>
2021-01-17 23:11:51 -0800

    enqueue

--- python/challenges/fifo_animal_shelter/fifo_animal_shelter/fifo_animal_shelter.py
+++ python/challenges/fifo_animal_shelter/fifo_animal_shelter/fifo_animal_shelter.py
@@ -6,3 +6,34 @@ class Node():
         self.value = value
         self.next = next
 
+class AnimalShelter():
+
+    def __init__(self, node = None):
+        self.front = node
+        self.rear = node
+
+    def enqueue(self, animal):
+        new_node = Node(animal)
+        if animal == "":
+            raise InvalidOperationError("Please input a valid animal.")
+        elif animal != "cat" and animal != "dog":
+            raise InvalidOperationError("Please input a valid animal.")
+
+        if self.is_empty() is True:
+            self.rear = new_node
+            self.front = new_node
+            return new_node
+        elif self.is_empty() is False:
+            self.rear.next = new_node
+            self.rear = new_node
+            return new_node
+
+
+
+
+
+    def is_empty(self):
+        if self.front is None and self.rear is None:
+            return True
+        else:
+            return False
--- python/challenges/fifo_animal_shelter/tests/test_fifo_animal_shelter.py
+++ python/challenges/fifo_animal_shelter/tests/test_fifo_animal_shelter.py
@@ -1,5 +1,15 @@
+from fifo_animal_shelter.fifo_animal_shelter import AnimalShelter, InvalidOperationError
+
 from fifo_animal_shelter import __version__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_enqueue_values():
+    q = AnimalShelter()
+    q.enqueue("cat")
+    q.enqueue("dog")
+    actual = q.front.value
+    expected = "cat"
+    assert actual == expected
