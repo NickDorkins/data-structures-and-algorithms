05a6de525e229d7352e160dd59e03eafd4cf7d1f
NickDorkins <nick.dorkins@gmail.com>
2021-02-09 04:52:20 -0800

    Fixed 'NoneType' object is not callable issue

--- python/challenges/fizz_buzz_tree/fizz_buzz_tree/fizz_buzz_tree.py
+++ python/challenges/fizz_buzz_tree/fizz_buzz_tree/fizz_buzz_tree.py
@@ -1,103 +1,94 @@
-from queue import Queue
+# from queue import Queue
 
 class Node:
-    def __init__(self, value, left = None, right = None):
+    def __init__(self, value = None, left = None, right = None):
         self.value = value
         self.left =  left
         self.right = right
+        # print('NODE TEST VALUE:', self.value)
+        # print('NODE TEST LEFT:', self.left)
+        # print('NODE TEST RIGHT:', self.right)
 
 class BinaryTree:
     
     def __init__(self, root = None):
         self.root = root
+        # print('BinTree TEST ROOT VALUE:', self.root)
+
 #  Write custom ERROR's for any that are raised
+    
+    
+    
     def preOrder(self):
         if self.root is None:
             return "Your Root is empty, please preLoad a root value."
+        # print('SELF ROOT TEST', self.root)
+        list_of_nodes_preorder = []
         def traverse(root):
-            if not root:
-                return
-            print(root.value)
-            traverse(root.left)
-            traverse(root.right)
+            list_of_nodes_preorder.append(root.value)
+        #     # if not root:
+        #     #     return 
+            # print('ROOT VALUE INSIDE PREORDER:', root.value)
+            if root.left:
+                traverse(root.left)
+                # print('ROOT LEFT VALUE INSIDE PREORDER:', root.left.value)
+            if root.right:
+                traverse(root.right)
+                # print('ROOT RIGHT VALUE INSIDE PREORDER:', root.right.value)
+            # print('RETURNED LIST:' ,list_of_nodes_preorder)
+        # print('ROOT VALUE INSIDE PREORDER END:', self.root)
+        return list_of_nodes_preorder
         traverse(self.root)
-    def inOrder(self):
-        if self.root is None:
-            return "Your Root is empty,inOrder to proceed, please add a root value."
-        def traverse(root):
-            if not root:
-                return
-            traverse(root.left)
-            print(root.value)
-            traverse(root.right)
-    def postOrder(self):
-        if self.root is None:
-            return "Your Root is empty, please add a root value."
-        def traverse(root):
-            if not root:
-                return
-            traverse(root.left)
-            traverse(root.right)
-            print(root.value)
-
-    def breadth_first_traversal(self):
-        if self.root is None:
-            return "Your Root is empty, please add a root value. (Breadth First Traversal)" 
-        
-        q = Queue()
-        q.enqueue(self.root)
-        traversal_order = []
-
-        while len(q) > 0:
-            current = q.dequeue()
-            if current is None:
-                return traversal_order
-            traversal_order.append(current.value)
-
-            if current.left != None:
-                q.enqueue(current.left.value)
-
-            if current.right != None:
-                q.enqueue(current.right.value)
-        
-        return traversal_order
-
     
-    def FizzBuzzTree(self):
-        if self.root is None:
-            return "Your Root is empty, please add a root value. (FizzBuzz)" 
-        
-        q = Queue()
-        q.enqueue(self.root)
+
+def FizzBuzzTree(self):
+    
+    if self.root is None:
+        return "Your Root is empty, please add a root value. (FizzBuzz)" 
+
         fizz_buzz_list = []
 
-        while len(q) > 0:
-            current = q.dequeue()
-            left = current.left
-            right = current.right
+        if left != None:
+            if left %15 == 0:
+                q.enqueue('FIZZBUZZ')
+            elif left %5 == 0:
+                q.enqueue('BUZZ')
+            elif left %3 == 0:
+                q.enqueue('FIZZ')
+            else:
+                q.enqueue(current.left.value)
 
-            if current is None:
-                return fizz_buzz_list
-            fizz_buzz_list.append(current.value)
+        if current.right != None:
+            if right %15 == 0:
+                q.enqueue('FIZZBUZZ')
+            elif right %5 == 0:
+                q.enqueue('BUZZ')
+            elif right %3 == 0:
+                q.enqueue('FIZZ')
+            else:
+                q.enqueue(current.right.value)
+    
+    return fizz_buzz_list
 
-            if left != None:
-                if left %15 == 0:
-                    q.enqueue('FIZZBUZZ')
-                elif left %5 == 0:
-                    q.enqueue('BUZZ')
-                elif left %3 == 0:
-                    q.enqueue('FIZZ')
-                else:
-                    q.enqueue(current.left.value)
 
-            if current.right != None:
-                if right %15 == 0:
-                    q.enqueue('FIZZBUZZ')
-                elif right %5 == 0:
-                    q.enqueue('BUZZ')
-                elif right %3 == 0:
-                    q.enqueue('FIZZ')
-                else:
-                    q.enqueue(current.right.value)
-        
-        return fizz_buzz_list
+
+if __name__ == "__main__":
+    
+    one = Node(1)
+    three = Node(3)
+    five = Node(5)
+    eight = Node(8)
+    ten = Node(10)
+    fifteen = Node(15)
+    sixty = Node(60)
+
+
+    tree = BinaryTree(one)
+    tree.root.left = three
+    tree.root.right = five
+    tree.root.left.left = eight
+    tree.root.left.right = ten
+    tree.root.right.left = fifteen
+    tree.root.right.right = sixty
+
+    tree.preOrder()
--- python/challenges/fizz_buzz_tree/tests/test_fizz_buzz_tree.py
+++ python/challenges/fizz_buzz_tree/tests/test_fizz_buzz_tree.py
@@ -1,5 +1,23 @@
-from fizz_buzz_tree import __version__
+
+from fizz_buzz_tree.fizz_buzz_tree import Node, BinaryTree, FizzBuzzTree
 
 
-def test_version():
-    assert __version__ == '0.1.0'
+
+
+# def test_return():
+#     tree = BinaryTree()
+#     tree.root(1)
+#     tree.root.left(3)
+#     tree.root.right(5)
+
+#     tree.root.left.left(8)
+#     tree.root.left.right(10)
+
+#     tree.root.right.left(15)
+#     tree.root.right.right(60)
+
+#     fizz_buzz_tree = FizzBuzzTree(tree)
+#     actual = fizz_buzz_tree.preorder
+#     expected = [1, 'FIZZ', 'BUZZ', ]
+#     assert actual == expected
+
